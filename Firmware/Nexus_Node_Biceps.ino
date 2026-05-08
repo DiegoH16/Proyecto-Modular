51285ae59fec1d59a987410245abaab9673ad15d
Gilfrond <aceves608@gmail.com>
2026-05-08 11:18:45 -0600

    Update Nexus_Node_Biceps.ino

--- Firmware/Nexus_Node_Biceps.ino
+++ Firmware/Nexus_Node_Biceps.ino
@@ -1,6 +1,16 @@
-/*
- * DISPOSITIVO BÍCEPS: MAX30105 PPG Sensor (V4.1 100Hz BROADCAST + TELEMETRÍA)
- * Función: Cliente (STA). Muestrea SpO2 a 100Hz y envía lotes por UDP.
+/* Copyright 2026 Diego Gutiérrez Hermosillo Medina, Obed Simón Aceves Gutiérrez
+    
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+    
+         http://www.apache.org/licenses/LICENSE-2.0
+    
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
  */
 #include <Wire.h>
 #include "MAX30105.h"
