be48c7570b67c551dafff4c5ef51dcf458182298
Gilfrond <aceves608@gmail.com>
2026-05-12 18:35:32 -0600

    Update AVA_Core_System.m

--- Dashboard/AVA_Core_System.m
+++ Dashboard/AVA_Core_System.m
@@ -12,6 +12,7 @@
      See the License for the specific language governing permissions and
      limitations under the License.
  */
+ 
  function AVA_Core_System()
     clearvars; clc; close all force;
     
